c70921fae7cd0489f19463979981d5a4df7cb986
Sam Protsenko <joe.skb7@gmail.com>
2019-11-11 19:14:47 +0200

    lecture17: dtbo: Sync the build script with lecture
    Signed-off-by: Sam Protsenko <joe.skb7@gmail.com>

--- lecture17-hw2/materials/module3/dtbo/build.sh
+++ lecture17-hw2/materials/module3/dtbo/build.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-input=hw3.dtso
+in=hw3.dtso
 gen=hw3_gen.dtso
-output=hw3.dtbo
+out=hw3.dtbo
 kernel_dir=~/repos/linux-stable
-gcc_flags="-E -P -x assembler-with-cpp -I$kernel_dir/include -o $gen"
-dtc_flags="-W no-unit_address_vs_reg -I dts -O dtb -o $output -b 0 -@"
+gcc_flags="-E -P -x assembler-with-cpp -I$kernel_dir/include"
+dtc_flags="-W no-unit_address_vs_reg -I dts -O dtb -b 0 -@"
 
-rm -f $output
-gcc $gcc_flags $input
-dtc $dtc_flags $gen
+rm -f $out
+gcc $gcc_flags -o $gen $in
+dtc $dtc_flags -o $out $gen
 rm -f $gen
